ac2112a9a4b4ad5bfe081155c64c5a00933bbf47
AAAA8866 <100890250+AAAA8866@users.noreply.github.com>
2022-03-22 17:08:47 +0900

    JEEP

---
+++ js/all_layout03.js
@@ -0,0 +1,38 @@
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icon = document.querySelector('.navbar__icon');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
+    
+});
+
+
+
+$(function() {
+    ///////////
+
+
+    $('.mainSlide').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:3000,
+        Speed:300,
+        dots:false
+    });
+
+    $('.mainVisual .slideArrows i:first-child').on('click',function() {
+        $('.mainSlide').slick('slickPrev')
+    });
+    $('.mainVisual .slideArrows i:last-child').on('click',function() {
+        $('.mainSlide').slick('slickNext')
+    });
+
+    function 팝업닫기 () {
+        $('.pop001').hide();
+    }
+    $('.pop button').on('click',팝업닫기);
+
+    ///////////
+})
